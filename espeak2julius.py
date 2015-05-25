5b58983385a801c8f5104d2ba59461df9f6ca26a
Taixzo <taixzo@gmail.com>
2015-05-25 18:44:25 -0400

    Improve matches, and discard unrecognizable song names

--- espeak2julius.py
+++ espeak2julius.py
@@ -4,9 +4,21 @@ import collections
 import random
 import os, sys
 from ID3 import ID3
+
+loglevel = 2
+
+def log(s, level):
+    if loglevel>=level:
+        print (s)
+
 syllconv = open('julius/espeak-julius.txt').read().splitlines()[1:]
 
+triphones = open ('julius/tiedlist').read()
+
 d = collections.OrderedDict()
+
+# substitutions = {'ix':'iy', 'r': 'er', 'er':'r', 'n':'ng','ey':'ay','aa':'ax','ax':'ae','ih':'ax','eh':'ix','z':'s','jh':'ch','aw':'ow'}
+substitutions = {'ix':'iy', 'r': 'er', 'er':'r', 'n':'ng','ey':'ay','aa':'ax','ax':'ae','ih':'ix','eh':'ix','z':'s','jh':'ch','aw':'ow','ey':'ix'}
 
 for i in syllconv:
     splitline = re.split("  +",i)
@@ -44,10 +56,50 @@ play      p l ey"""
     for i, string in enumerate(stringlist):
         words = string.replace('(','').replace(')','').replace('[','').replace(']','').replace('/',' ').replace('-',' ').split()
         c = [e2j(word) for word in words]
-        gram += "\nTITLE:"
+        tvoca = ""
+        tgram = "\nTITLE:"
         for index, pronunciation in enumerate(c):
-            voca += "\n\n% W_"+str(i)+"_"+str(index)+"\n"+words[index].lower().ljust(10)+' '+pronunciation
-            gram += " W_"+str(i)+"_"+str(index)
+            tobreak = False
+            tpro = pronunciation.split(' ')
+            if len(tpro)>2:
+                for k in range(len(tpro)-2):
+                    if words[index].lower()=='sorcery': print (tpro)
+                    if not "\n"+tpro[k]+'-'+tpro[k+1]+'+'+tpro[k+2] in triphones:
+                        if words[index].lower()=='sorcery': print (tpro[k]+'-'+tpro[k+1]+'+'+tpro[k+2]+" not in triphones")
+                        if tpro[k] in substitutions and "\n"+substitutions[tpro[k]]+'-'+tpro[k+1]+'+'+tpro[k+2] in triphones:
+                            log (words[index]+": substituted "+substitutions[tpro[k]]+" into "+tpro[k]+'-'+tpro[k+1]+'+'+tpro[k+2], 2)
+                            tpro[k] = substitutions[tpro[k]]
+                            continue
+                        elif tpro[k+1] in substitutions and "\n"+tpro[k]+'-'+substitutions[tpro[k+1]]+'+'+tpro[k+2] in triphones:
+                            log (words[index]+": substituted "+substitutions[tpro[k+1]]+" into: "+tpro[k]+'-'+tpro[k+1]+'+'+tpro[k+2], 2)
+                            tpro[k+1] = substitutions[tpro[k+1]]
+                            continue
+                        elif tpro[k+2] in substitutions and "\n"+tpro[k]+'-'+tpro[k+1]+'+'+substitutions[tpro[k+2]] in triphones:
+                            log (words[index]+": substituted "+substitutions[tpro[k+2]]+" into: "+tpro[k]+'-'+tpro[k+1]+'+'+tpro[k+2], 2)
+                            tpro[k+2] = substitutions[tpro[k+2]]
+                            continue
+                        print (words[index]+" will fail. Failing triphone: "+tpro[k]+'-'+tpro[k+1]+'+'+tpro[k+2])
+                        tobreak = True
+                        tvoca = ""
+                        tgram = ""
+                        break
+                    else:
+                        if words[index].lower()=='sorcery': print (tpro[k]+'-'+tpro[k+1]+'+'+tpro[k+2]+" passes")
+                if tobreak: break
+                # Second pass in case substitutions broke earlier triphones
+                for k in range(len(tpro)-2):
+                    if not "\n"+tpro[k]+'-'+tpro[k+1]+'+'+tpro[k+2] in triphones:
+                        print (words[index]+" failed second pass. Failing triphone: "+tpro[k]+'-'+tpro[k+1]+'+'+tpro[k+2])
+                        tobreak = True
+                        tvoca = ""
+                        tgram = ""
+                        break
+                pronunciation = ' '.join(tpro)
+            if tobreak: break
+            tvoca += "\n\n% W_"+str(i)+"_"+str(index)+"\n"+words[index].lower().ljust(10)+' '+pronunciation
+            tgram += " W_"+str(i)+"_"+str(index)
+        voca += tvoca
+        gram += tgram
     voca = voca.replace('ih \n','iy \n').replace(' n k ',' ng k ')
     if not os.path.exists('/tmp/saera'):
         os.mkdir('/tmp/saera')
@@ -83,4 +135,4 @@ if __name__=="__main__":
         else:
             continue
     # print (lst)
-    print (create_grammar(lst))
+    create_grammar(lst)
